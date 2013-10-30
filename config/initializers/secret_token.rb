976a58012e7d77f4b16b87d6eeb2452355aef982
schneems <richard.schneeman@gmail.com>
2013-10-29 21:38:22 -0500

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SprocketsNoDigestUpdate::Application.config.secret_key_base = '03cadfe3d5b057bb4dd4e0dacffddfe76b580ebc4107234876bedc2539fa34b9c8c628c23d21d99342f98b47f08a72e13d1f175c6316a92728767636fff9f284'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SprocketsNoDigestUpdate::Application.config.session_store :cookie_store, key: '_sprockets-no-digest-update_session'
